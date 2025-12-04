cdee61b7d064a833075017674beb3343cc718665
Vouchyk <user@users-MacBook-Air.local>
2025-12-04 14:39:39 +0000

    Edit server part

--- src/api/newsApi.ts
+++ src/api/newsApi.ts
@@ -1,20 +1,14 @@
-const API_KEY = "2dbff87b77474bef8503a2ba15e4c678";
-const BASE = "https://newsapi.org/v2";
-import type { NewsApiResponse } from "../types/news";
-
-export const getHeadlines = async (): Promise<NewsApiResponse> => {
-  const res = await fetch(`${BASE}/top-headlines?country=us&apiKey=${API_KEY}`);
+export const getHeadlines = async () => {
+  const res = await fetch("http://localhost:5000/api/headlines");
   return res.json();
 };
 
 export const getByCategory = async (category: string) => {
-  const res = await fetch(
-    `${BASE}/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`
-  );
+  const res = await fetch(`http://localhost:5000/api/category/${category}`);
   return res.json();
 };
 
 export const searchNews = async (query: string) => {
-  const res = await fetch(`${BASE}/everything?q=${query}&apiKey=${API_KEY}`);
+  const res = await fetch(`http://localhost:5000/api/search/${query}`);
   return res.json();
 };
