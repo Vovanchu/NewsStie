bb464c064ca47603033b8e3d8daf7d0b0201a2ed
Vouchyk <user@users-MacBook-Air.local>
2025-12-04 14:45:04 +0000

    Edit backend JSON

--- Backend/server.js
+++ Backend/server.js
@@ -35,4 +35,5 @@ app.get("/api/search/:query", async (req, res) => {
   res.json(data);
 });
 
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
