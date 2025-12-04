afc96c6b1540d6e7adf281fae4044cb8f7ff0f47
Vouchyk <user@users-MacBook-Air.local>
2025-12-04 14:02:29 +0000

    Edit routes in site

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -7,6 +7,7 @@ import { Error } from "../pages/Error";
 
 export const AppRouter = () => (
   <Routes>
+    <Route path={"/"} element={<Home />} />
     <Route path={routes.home} element={<Home />} />
     <Route path={routes.category} element={<Category />} />
     <Route path={routes.search} element={<Search />} />
