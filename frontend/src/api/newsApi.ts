926996a00299e9b5d7a3acfde03a965d4ecbbae2
Vouchyk <user@users-MacBook-Air.local>
2025-12-04 16:30:20 +0000

    Edit base url for frontend

--- frontend/src/api/newsApi.ts
+++ frontend/src/api/newsApi.ts
@@ -1,5 +1,5 @@
 const BASE_URL = import.meta.env.PROD
-  ? "https://news-backend-2pdm.onrender.com"
+  ? import.meta.env.VITE_SERVER_API_URL
   : import.meta.env.VITE_API_URL;
 
 console.log("BASE_URL:", BASE_URL);
